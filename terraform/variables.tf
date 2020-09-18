f66d9d6a4f3914a977fbd04138dfa74755db3032
Vaibhavarora08 <38854840+Vaibhavarora08@users.noreply.github.com>
2020-09-18 21:52:31 +1000

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "ami" {}
+variable "key_name" {}
+variable "web_instance_type" {}
+variable "db_instance_type" {}
+variable "aws_profile" {}
+variable "public_key_path" {}
+variable "mw_vpc"{}
+variable "my_igw"{}
